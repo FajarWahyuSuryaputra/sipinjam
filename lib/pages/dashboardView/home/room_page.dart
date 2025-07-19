e8814ea14a83988a739ba8e9f00fc70d59853701
FajarWahyuSuryaputra <fsuryaputra028@gmail.com>
2025-07-19 19:39:08 +0700

    animation for expanded roomList

--- lib/pages/dashboardView/home/room_page.dart
+++ lib/pages/dashboardView/home/room_page.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:sipinjam/config/app_colors.dart';
-import 'package:sipinjam/config/nav.dart';
 import 'package:sipinjam/datasources/ruangan_datasource.dart';
 import 'package:sipinjam/models/gedung_model.dart';
 import 'package:sipinjam/models/ruangan_model.dart';
-import 'package:sipinjam/pages/dashboardView/home/room_detail_page.dart';
 import 'package:sipinjam/providers/ruangan_provider.dart';
 
 import '../../../config/app_constans.dart';
@@ -80,107 +78,103 @@ class _RoomPageState extends ConsumerState<RoomPage> {
         padding: const EdgeInsets.fromLTRB(20, 40, 20, 0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            header(context),
-            Expanded(
-              child: Consumer(
-                builder: (_, wiRef, __) {
-                  List<RuanganModel> ruanganList = wiRef.watch(ruanganProvider);
-                  final expandedIndex = wiRef.watch(expandedRoomProvider);
-                  return StaggeredGrid.count(
-                      crossAxisCount: 2,
-                      mainAxisSpacing: 8,
-                      crossAxisSpacing: 8,
-                      children: List.generate(
-                        ruanganList.length,
-                        (index) {
-                          RuanganModel ruangan = ruanganList[index];
-                          final isExpanded = index == expandedIndex;
-                          final imgSelected =
-                              wiRef.watch(imgSelectedProvider(index));
-                          if (imgSelected.isEmpty &&
-                              ruangan.fotoRuangan!.isNotEmpty) {
-                            WidgetsBinding.instance.addPostFrameCallback((_) {
-                              setImgSelected(ref, index,
-                                  '${AppConstans.imageUrl}${ruangan.fotoRuangan!.first.replaceAll('../../../api/assets/', '/')}');
-                            });
-                          }
-
-                          return StaggeredGridTile.count(
-                              crossAxisCellCount: isExpanded ? 2 : 1,
-                              mainAxisCellCount: isExpanded ? 2 : 1,
-                              child: GestureDetector(
-                                  onTap: () {
-                                    setExpandedRoom(
-                                        ref, isExpanded ? null : index);
-                                  },
-                                  child: AnimatedSwitcher(
-                                    duration: const Duration(seconds: 5),
-                                    switchInCurve: Curves.easeInOut,
-                                    switchOutCurve: Curves.easeInOut,
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius: BorderRadius.circular(12),
-                                        boxShadow: [
-                                          BoxShadow(
-                                            color:
-                                                Colors.black.withOpacity(0.05),
-                                            blurRadius: 6,
-                                            offset: const Offset(0, 2),
-                                          )
-                                        ],
-                                      ),
-                                      child: Column(
-                                        key: ValueKey(isExpanded
-                                            ? 'expanded_$index'
-                                            : 'default_$index'),
-                                        children: [
-                                          Expanded(
-                                            child: Container(
-                                              decoration: BoxDecoration(
-                                                  borderRadius:
-                                                      BorderRadius.circular(12),
-                                                  image: DecorationImage(
-                                                      fit: BoxFit.cover,
-                                                      image: NetworkImage(
-                                                        isExpanded &&
-                                                                imgSelected
-                                                                    .isNotEmpty
-                                                            ? imgSelected
-                                                                .replaceAll(
-                                                                    '../../../api/assets/',
-                                                                    '/')
-                                                            : '${AppConstans.imageUrl}${ruangan.fotoRuangan!.first.replaceAll('../../../api/assets/', '/')}',
-                                                      ))),
-                                            ),
-                                          ),
-                                          isExpanded
-                                              ? roomExpandedInformation(
-                                                  ruangan,
-                                                  wiRef,
-                                                  expandedIndex,
-                                                  imgSelected)
-                                              : roomDefaultInformation(ruangan)
-                                        ],
-                                      ),
-                                    ),
-                                  )));
-                        },
-                      ));
-                },
-              ),
-            )
-          ],
+          children: [header(context), roomList()],
         ),
       ),
     );
   }
 
+  Expanded roomList() {
+    return Expanded(
+      child: Consumer(
+        builder: (_, wiRef, __) {
+          List<RuanganModel> ruanganList = wiRef.watch(ruanganProvider);
+          final expandedIndex = wiRef.watch(expandedRoomProvider);
+          return StaggeredGrid.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 8,
+              crossAxisSpacing: 8,
+              children: List.generate(
+                ruanganList.length,
+                (index) {
+                  RuanganModel ruangan = ruanganList[index];
+                  final isExpanded = index == expandedIndex;
+                  final imgSelected = wiRef.watch(imgSelectedProvider(index));
+                  if (imgSelected.isEmpty && ruangan.fotoRuangan!.isNotEmpty) {
+                    WidgetsBinding.instance.addPostFrameCallback((_) {
+                      setImgSelected(ref, index,
+                          '${AppConstans.imageUrl}${ruangan.fotoRuangan!.first.replaceAll('../../../api/assets/', '/')}');
+                    });
+                  }
+
+                  return StaggeredGridTile.count(
+                      crossAxisCellCount: isExpanded ? 2 : 1,
+                      mainAxisCellCount: isExpanded ? 2 : 1,
+                      child: GestureDetector(
+                          onTap: () {
+                            setExpandedRoom(ref, isExpanded ? null : index);
+                          },
+                          child: AnimatedSwitcher(
+                            duration: const Duration(milliseconds: 400),
+                            switchInCurve: Curves.easeInOut,
+                            switchOutCurve: Curves.easeInOut,
+                            child: Container(
+                              key: ValueKey(isExpanded
+                                  ? 'expanded_$index'
+                                  : 'default_$index'),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(12),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.05),
+                                    blurRadius: 6,
+                                    offset: const Offset(0, 2),
+                                  )
+                                ],
+                              ),
+                              child: Column(
+                                children: [
+                                  Expanded(
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(12),
+                                          image: DecorationImage(
+                                              fit: BoxFit.cover,
+                                              image: NetworkImage(
+                                                isExpanded &&
+                                                        imgSelected.isNotEmpty
+                                                    ? imgSelected.replaceAll(
+                                                        '../../../api/assets/',
+                                                        '/')
+                                                    : '${AppConstans.imageUrl}${ruangan.fotoRuangan!.first.replaceAll('../../../api/assets/', '/')}',
+                                              ))),
+                                    ),
+                                  ),
+                                  isExpanded
+                                      ? roomExpandedInformation(
+                                          ruangan,
+                                          wiRef,
+                                          expandedIndex,
+                                          isExpanded,
+                                          imgSelected)
+                                      : roomDefaultInformation(ruangan)
+                                ],
+                              ),
+                            ),
+                          )));
+                },
+              ));
+        },
+      ),
+    );
+  }
+
   Padding roomExpandedInformation(RuanganModel ruangan, WidgetRef wiRef,
-      int? expandedIndex, String imgSelected) {
+      int? expandedIndex, bool isExpaded, String imgSelected) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -190,27 +184,29 @@ class _RoomPageState extends ConsumerState<RoomPage> {
               Text(
                 ruangan.namaRuangan,
                 overflow: TextOverflow.ellipsis,
-                style:
-                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                style: TextStyle(
+                    fontSize: isExpaded ? 18 : 12, fontWeight: FontWeight.bold),
               ),
               Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   const Icon(
                     Icons.people,
-                    // size: 20,
                   ),
-                  const SizedBox(
-                    width: 3,
-                  ),
-                  Text(ruangan.kapasitas.toString())
+                  Text(
+                    ruangan.kapasitas.toString(),
+                    overflow: TextOverflow.ellipsis,
+                  )
                 ],
               )
             ],
           ),
-          Text(ruangan.namaGedung),
+          Text(
+            ruangan.namaGedung,
+            overflow: TextOverflow.ellipsis,
+          ),
           const SizedBox(
-            height: 4,
+            height: 2,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -229,14 +225,14 @@ class _RoomPageState extends ConsumerState<RoomPage> {
                   ),
                   Text(
                     fasilitas.trim(),
-                    style: const TextStyle(fontSize: 16),
+                    style: TextStyle(fontSize: isExpaded ? 16 : 12),
                   ),
                 ],
               );
             }).toList(),
           ),
           const SizedBox(
-            height: 4,
+            height: 2,
           ),
           SizedBox(
             height: 60,
@@ -254,7 +250,6 @@ class _RoomPageState extends ConsumerState<RoomPage> {
                 return GestureDetector(
                   onTap: () {
                     setImgSelected(ref, expandedIndex, imgUrl);
-                    print(imgSelected);
                   },
                   child: Container(
                     margin: const EdgeInsets.symmetric(horizontal: 4),
